e77727509fc00c4d47803d40dabe0cd5b5f15206
sudiplamatamang <sudiptamang123>
2020-01-10 22:56:47 +0545

    created url patterns for search function

--- FutsalManagementSystem/events/models.py
+++ FutsalManagementSystem/events/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Event(models.Model):
     event_name = models.CharField('Event Name',max_length=100)
     venue = models.CharField('Venue',max_length=100)
-    event_date = models.DateTimeField('Event Date')
+    event_date = models.DateField('Event Date')
     manager = models.CharField('Manager Name', max_length=100)
-    event_description=models.CharField('Event Description',max_length=200)    
+    
