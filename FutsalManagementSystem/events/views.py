b94dd28ef79674b258cf9a68b4786a7ee9dac592
sudiplamatamang <sudiptamang123>
2020-01-10 23:30:15 +0545

    updated search page and created view function for search function

--- FutsalManagementSystem/events/views.py
+++ FutsalManagementSystem/events/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
 
+def search(request):
+    if request.method=='POST':
+        srch = request.POST['srh']
+        if srch:
+            match = Event.objects.filter(Q(event_name__icontains=srch) |
+                                         Q(venue__icontains=srch)
+                                         )
+            if match:
+                return render(request,'searchdata.html', {'sr':match})
+            else:
+                messages.error(request,'no result found')
+        else:
+            return HttpResponseRedirect('/search/')
+    return render(request,'searchdata.html')
+
 # Create your views here.
 
