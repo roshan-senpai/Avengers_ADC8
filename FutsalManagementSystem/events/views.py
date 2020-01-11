927d2d0760269cc1b8b0044a5da1bb5ff8887a64
Roshan Shrestha <roshanshretha768@gmail.com>
2020-01-11 10:46:26 +0545

    created view function to view events

--- FutsalManagementSystem/events/views.py
+++ FutsalManagementSystem/events/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .models import Event
+from django.http import HttpResponse
 
 # Create your views here.
-
+def view_event_data(request):
+    events_list=Event.objects.all()
+    con_var={
+        'events':events_list
+    }
+    return render(request,'event/viewevent.html',con_var)
