4600a5ff5784c3b7a988ebaca25f4e2966b2da70
ronit-debug <58837022+ronit-debug@users.noreply.github.com>
2020-01-11 17:25:09 +0545

    added media_root and media_url

--- FutsalManagementSystem/FutsalManagementSystem/settings.py
+++ FutsalManagementSystem/FutsalManagementSystem/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'FutsalManagementSystem.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'events/templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
