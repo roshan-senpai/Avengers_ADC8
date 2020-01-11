e218e547d1f85e9f42475bd2a3a7911834c19ad7
ronit-debug <58837022+ronit-debug@users.noreply.github.com>
2020-01-11 17:25:54 +0545

    created view function for upload

--- FutsalManagementSystem/events/views.py
+++ FutsalManagementSystem/events/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 
+
+def upload(request):
+    if request.method == 'POST':
+        uploaded_file = request.FILES['doc']
+        file_object = FileSystemStorage()
+        file_object.save(uploaded_file.name, uploaded_file)
+    return render(request,'upload.html')
